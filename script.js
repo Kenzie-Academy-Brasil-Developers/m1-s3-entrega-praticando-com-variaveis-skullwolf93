799a700da8c5b65e9d8d2a680f11052a720d5aa2
Thiago <thvscs93@gmail.com>
2022-08-09 15:44:32 -0300

    entrega 1 sprint 3

---
+++ script.js
@@ -0,0 +1,11 @@
+let nome = "Thiago"
+
+let sobreNome = " Vasconcelos"
+
+let anoNascimento = 1993
+
+let idade = (2022 - anoNascimento)
+
+let cidade = "Curitiba"
+
+console.log(`Eu sou o ${nome}${sobreNome} tenho ${idade} anos e moro em ${cidade}`)
